e3db289467ee54f7a2ef6065d7584bb6ba7a4282
pyvyx <pyvyx@web.de>
2022-11-20 22:15:57 +0100

    Moved settings window into a seperate class

---
+++ MouseTracker/src/SettingsWindow.h
@@ -0,0 +1,207 @@
+#pragma once
+#include <filesystem>
+#include <algorithm>
+#include <optional>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <vector>
+#include <Windows.h>
+
+#include "ImGui/imgui.h"
+#include "nfd/nfd.h"
+
+#include "Monitor.h"
+#include "Window.h"
+#include "Image.h"
+
+#define CURSOR_POS(cPos, mPos) (cPos - mPos)
+
+class SettingsWindow
+{
+private:
+    bool m_Tracking = false;
+    bool m_BigPixelMode = false;
+    bool m_SleepWhileIdle = true;
+    size_t m_SelectedMonitor = 0;
+    Image& m_rImage;
+private:
+    static inline void PushStyleColors()
+    {
+        static constexpr ImVec4 dark{ 0.27f, 0.27f, 0.27f, 1.0f };
+        static constexpr ImVec4 grey{ 0.57f, 0.57f, 0.57f, 1.0f };
+        static constexpr ImVec4 lightGrey{ 0.70f, 0.70f, 0.70f, 1.0f };
+        ImGui::PushStyleColor(ImGuiCol_Button, dark);
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, grey);
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, grey);
+        ImGui::PushStyleColor(ImGuiCol_FrameBg, dark);
+        ImGui::PushStyleColor(ImGuiCol_FrameBgHovered, grey);
+        ImGui::PushStyleColor(ImGuiCol_Header, dark);
+        ImGui::PushStyleColor(ImGuiCol_HeaderHovered, grey);
+        ImGui::PushStyleColor(ImGuiCol_HeaderActive, dark);
+        ImGui::PushStyleColor(ImGuiCol_CheckMark, lightGrey);
+        ImGui::PushStyleColor(ImGuiCol_FrameBgActive, grey);
+    }
+
+    // not using glfw since it doesn't record key presses if window isn't focused
+    inline bool KeyPressed(int key) const
+    {
+        return GetAsyncKeyState(key) & 0x01;
+    }
+
+    inline std::optional<std::filesystem::path> GetSavePath() const
+    {
+        nfdchar_t* savePath = NULL;
+        nfdresult_t result = NFD_SaveDialog("png", NULL, &savePath);
+        if (result == NFD_OKAY)
+        {
+            const std::filesystem::path path = savePath;
+            free(savePath);
+            Log << "GetSavePath() path: [" << path << ']' << std::endl;
+            return { path };
+        }
+        else if (result == NFD_CANCEL)
+            Log << "GetSavePath() user pressed cancel" << std::endl;
+        else
+        {
+            const std::string errorMsg = "Failed to open file [" + std::string(savePath) + "]";
+            Err << "GetSavePath() " << errorMsg << std::endl;
+            MsgBoxError(errorMsg.c_str());
+        }
+        return std::nullopt;
+    }
+
+
+    inline std::optional<std::string> GetImagePath() const
+    {
+        nfdchar_t* outPath = NULL;
+        nfdresult_t result = NFD_OpenDialog("png,jpeg,jpg", NULL, &outPath);
+        if (result == NFD_OKAY)
+        {
+            const std::string filePath = outPath;
+            free(outPath);
+            Log << "GetImagePath() path: [" << filePath << ']' << std::endl;
+            return { filePath };
+        }
+        else if (result == NFD_CANCEL)
+            Log << "GetImagePath() user pressed cancel" << std::endl;
+        else
+        {
+            const std::string errorMsg = "Failed to open file [" + std::string(outPath) + "]";
+            Err << "GetImagePath() " << errorMsg << std::endl;
+            MsgBoxError(errorMsg.c_str());
+        }
+        return std::nullopt;
+    }
+
+
+    inline void SaveImage(const Image& img) const
+    {
+        const std::optional<std::filesystem::path> path = GetSavePath();
+        if (!path.has_value())
+            return;
+        if (!img.WriteToFile(path.value()))
+        {
+            const std::string errorMsg = "Failed to write image [" + path.value().string() + "]";
+            MsgBoxError(errorMsg.c_str());
+        }
+    }
+
+
+    inline void LoadImg(Image& img) const
+    {
+        std::optional<std::string> path = GetImagePath();
+        if (!path.has_value())
+            return;
+        img.LoadFromFile(path.value());
+    }
+
+
+    inline std::string ConcatSelection(const std::vector<MonitorInfo>& mInfo) const
+    {
+        std::string selection;
+        for (const MonitorInfo& i : mInfo)
+        {
+            std::string tmp;
+            std::transform(i.name.begin(), i.name.end(), std::back_inserter(tmp), [](wchar_t c) { return (char)c; });
+            selection += tmp + '\0';
+        }
+        return selection;
+    }
+
+
+    inline void SetWindowUp(ImVec2 wSize) const
+    {
+        PushStyleColors();
+        ImGui::Begin("Settings", NULL, IMGUI_WINDOW_FLAGS);
+        ImGui::SetWindowPos({ 0, 0 });
+        ImGui::SetWindowSize({ wSize.x, wSize.y * (1.f / 4.f) });
+    }
+public:
+    inline explicit SettingsWindow(Image& img) : m_rImage(img) {}
+
+    inline void Show(ImVec2 wSize, POINT pos, const std::vector<MonitorInfo>& mInfo)
+    {
+        SetWindowUp(wSize);
+        ImGui::LabelText("Resolution", "%dx%d", mInfo[m_SelectedMonitor].w, mInfo[m_SelectedMonitor].h);
+        ImGui::LabelText("Cursor position", "x=%ld y=%ld", CURSOR_POS(pos.x, mInfo[m_SelectedMonitor].x), CURSOR_POS(pos.y, mInfo[m_SelectedMonitor].y));
+
+        const size_t prevMonitor = m_SelectedMonitor;
+        static const std::string mSelection = ConcatSelection(mInfo);
+        if (ImGui::Combo("##Monitor", (int*)&m_SelectedMonitor, mSelection.c_str()))
+        {
+            const MonitorInfo& pm = mInfo[prevMonitor];
+            const MonitorInfo& sm = mInfo[m_SelectedMonitor];
+            if (pm.w != sm.w || pm.h != sm.h)
+            {
+                if (MsgBoxWarning("Changing the monitor will cause the image to be lost because the monitors have different resolutions! Do you want to continue?") == IDYES)
+                {
+                    m_rImage.Resize(sm.w, sm.h);
+                    if (m_SelectedMonitor == mInfo.size() - 1)
+                    {
+                        m_rImage.SetAllPixel(80);
+                        for (size_t i = 0; i < mInfo.size() - 1; ++i)
+                        {
+                            m_rImage.SetPixelRange(CURSOR_POS(mInfo[i].x, mInfo.back().x), CURSOR_POS(mInfo[i].y, mInfo.back().y), mInfo[i].w, mInfo[i].h, 255);
+                        }
+                    }
+                }
+                else
+                    m_SelectedMonitor = prevMonitor;
+            }
+        }
+
+        if (ImGui::RadioButton("Sleep while idle [F7]", m_SleepWhileIdle) || KeyPressed(VK_F7))
+            m_SleepWhileIdle = !m_SleepWhileIdle;
+
+        if (ImGui::RadioButton("Big pixel mode [F8]", m_BigPixelMode) || KeyPressed(VK_F8))
+            m_BigPixelMode = !m_BigPixelMode;
+
+        if (m_Tracking)
+            ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(0, 230, 0, 255));
+        else
+            ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(230, 0, 0, 255));
+        if (ImGui::RadioButton("Tracking [F9]", m_Tracking) || KeyPressed(VK_F9))
+            m_Tracking = !m_Tracking;
+        ImGui::PopStyleColor();
+
+        if (ImGui::Button("Save image", { 104,0 }))
+            SaveImage(m_rImage);
+
+        const float loadImageX = ImGui::GetItemRectSize().x + 20.f;
+        ImGui::SameLine(loadImageX);
+        if (ImGui::Button("Load image"))
+            LoadImg(m_rImage);
+
+        ImGui::SameLine(loadImageX + ImGui::GetItemRectSize().x + 10);
+        if (ImGui::Button("Reset image") && MsgBoxWarning("Do you really want to reset the tracking image? This change can't be undone!") == IDYES)
+            m_rImage.Reset();
+        ImGui::PopStyleColor(10);
+        ImGui::End();
+    }
+
+    constexpr bool Tracking()          const { return m_Tracking;        }
+    constexpr bool BigPixelMode()      const { return m_BigPixelMode;    }
+    constexpr bool SleepWhileIdle()    const { return m_SleepWhileIdle;  }
+    constexpr size_t SelectedMonitor() const { return m_SelectedMonitor; }
+};
--- MouseTracker/src/main.cpp
+++ MouseTracker/src/main.cpp
@@ -1,7 +1,4 @@
-#include <filesystem>
-#include <iostream>
-#include <optional>
-#include <cstring>
+#include <cstdlib>
 #include <string>
 #include <thread>
 #include <chrono>
@@ -10,24 +7,17 @@
 #include "ImGui/imgui.h"
 #include "nfd/nfd.h"
 
+#include "SettingsWindow.h"
 #include "Window.h"
 #include "Monitor.h"
 #include "Clang.h"
 #include "Image.h"
 #include "Log.h"
 
-#define CURSOR_POS(cPos, mPos) (cPos - mPos)
-
-// not using glfw since it doesn't record key presses if window isn't focused
-inline bool KeyPressed(int key)
-{
-    return GetAsyncKeyState(key) & 0x01;
-}
-
 inline void ImageWindow(ImVec2 wSize, const Image& image)
 {
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
-    ImGui::Begin("Image", (bool*)0, IMGUI_WINDOW_FLAGS);
+    ImGui::Begin("Image", NULL, IMGUI_WINDOW_FLAGS);
     ImGui::SetWindowPos({ 0, wSize.y * (1.f / 4.f) });
     ImGui::SetWindowSize({ wSize.x, wSize.y * (3.f / 4.f)});
 
@@ -54,161 +44,6 @@ inline void ImageWindow(ImVec2 wSize, const Image& image)
 }
 
 
-inline std::optional<std::filesystem::path> GetSavePath()
-{
-    nfdchar_t* savePath = NULL;
-    nfdresult_t result = NFD_SaveDialog("png", NULL, &savePath);
-    if (result == NFD_OKAY)
-    {
-        const std::filesystem::path path = savePath;
-        free(savePath);
-        Log << "GetSavePath() path: [" << path << ']' << std::endl;
-        return { path };
-    }
-    else if (result == NFD_CANCEL)
-        Log << "GetSavePath() user pressed cancel" << std::endl;
-    else
-    {
-        const std::string errorMsg = "Failed to open file [" + std::string(savePath) + "]";
-        Err << "GetSavePath() " << errorMsg << std::endl;
-        MsgBoxError(errorMsg.c_str());
-    }
-    return std::nullopt;
-}
-
-
-inline std::optional<std::string> GetImagePath()
-{
-    nfdchar_t* outPath = NULL;
-    nfdresult_t result = NFD_OpenDialog("png,jpeg,jpg", NULL, &outPath);
-    if (result == NFD_OKAY)
-    {
-        const std::string filePath = outPath;
-        free(outPath);
-        Log << "GetImagePath() path: [" << filePath << ']' << std::endl;
-        return { filePath };
-    }
-    else if (result == NFD_CANCEL)
-        Log << "GetImagePath() user pressed cancel" << std::endl;
-    else
-    {
-        const std::string errorMsg = "Failed to open file [" + std::string(outPath) + "]";
-        Err << "GetImagePath() " << errorMsg << std::endl;
-        MsgBoxError(errorMsg.c_str());
-    }
-    return std::nullopt;
-}
-
-
-inline void SaveImage(const Image& img)
-{
-    const std::optional<std::filesystem::path> path = GetSavePath();
-    if (!path.has_value())
-        return;
-    if (!img.WriteToFile(path.value()))
-    {
-        const std::string errorMsg = "Failed to write image [" + path.value().string() + "]";
-        MsgBoxError(errorMsg.c_str());
-    }
-}
-
-
-inline void LoadImg(Image& img)
-{
-    std::optional<std::string> path = GetImagePath();
-    if (!path.has_value())
-        return;
-    img.LoadFromFile(path.value());
-}
-
-
-inline std::string ConcatSelection(const std::vector<MonitorInfo>& mInfo)
-{
-    std::string selection;
-    for (const MonitorInfo& i : mInfo)
-    {
-        std::string tmp;
-        std::transform(i.name.begin(), i.name.end(), std::back_inserter(tmp), [](wchar_t c) { return (char)c; });
-        selection += tmp + '\0';
-    }
-    return selection;
-}
-
-
-inline void SettingsWindow(ImVec2 wSize, POINT pos, const std::vector<MonitorInfo>& mInfo, bool& tracking, bool& bigPixelMode, bool& sleepWhileIdle, Image& img, size_t& selectedMonitor)
-{
-    ImGui::Begin("Settings", (bool*)0, IMGUI_WINDOW_FLAGS);
-    ImGui::PushStyleColor(ImGuiCol_Button, { 0.27f, 0.27f, 0.27f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, { 0.57f, 0.57f, 0.57f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_ButtonActive, { 0.57f, 0.57f, 0.57f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_FrameBg, { 0.27f, 0.27f, 0.27f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_FrameBgHovered, { 0.57f, 0.57f, 0.57f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_Header, { 0.27f, 0.27f, 0.27f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_HeaderHovered, { 0.57f, 0.57f, 0.57f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_HeaderActive, { 0.27f, 0.27f, 0.27f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_CheckMark, { 0.7f,   0.7f,   0.7f, 1.0f });
-    ImGui::PushStyleColor(ImGuiCol_FrameBgActive, { 0.57f, 0.57f, 0.57f, 1.0f });
-    ImGui::SetWindowPos({ 0, 0 });
-    ImGui::SetWindowSize({ wSize.x, wSize.y * (1.f / 4.f) });
-
-    ImGui::LabelText("Resolution", "%dx%d", mInfo[selectedMonitor].w, mInfo[selectedMonitor].h);
-    ImGui::LabelText("Cursor position", "x=%ld y=%ld", CURSOR_POS(pos.x, mInfo[selectedMonitor].x), CURSOR_POS(pos.y, mInfo[selectedMonitor].y));
-    
-    const size_t prevMonitor = selectedMonitor;
-    static const std::string mSelection = ConcatSelection(mInfo);
-    if (ImGui::Combo("##Monitor", (int*)&selectedMonitor, mSelection.c_str()))
-    {
-        const MonitorInfo& pm = mInfo[prevMonitor];
-        const MonitorInfo& sm = mInfo[selectedMonitor];
-        if (pm.w != sm.w || pm.h != sm.h)
-        {
-            if (MsgBoxWarning("Changing the monitor will cause the image to be lost because the monitors have different resolutions! Do you want to continue?") == IDYES)
-            {
-                img.Resize(sm.w, sm.h);
-                if (selectedMonitor == mInfo.size() - 1)
-                {
-                    img.SetAllPixel(80);
-                    for (size_t i = 0; i < mInfo.size() - 1; ++i)
-                    {
-                        img.SetPixelRange(CURSOR_POS(mInfo[i].x, mInfo.back().x), CURSOR_POS(mInfo[i].y, mInfo.back().y), mInfo[i].w, mInfo[i].h, 255);
-                    }
-                }
-            }
-            else
-                selectedMonitor = prevMonitor;
-        }
-    }
-    
-    if (ImGui::RadioButton("Sleep while idle [F7]", sleepWhileIdle))
-        sleepWhileIdle = !sleepWhileIdle;
-
-    if (ImGui::RadioButton("Big pixel mode [F8]", bigPixelMode))
-        bigPixelMode = !bigPixelMode;
-
-    if(tracking)
-        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(0, 230, 0, 255));
-    else
-        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(230, 0, 0, 255));
-    if (ImGui::RadioButton("Tracking [F9]", tracking))
-        tracking = !tracking;
-    ImGui::PopStyleColor();
-
-    if (ImGui::Button("Save image", {104,0}))
-        SaveImage(img);
-
-    const float loadImageX = ImGui::GetItemRectSize().x + 20.f;
-    ImGui::SameLine(loadImageX);
-    if (ImGui::Button("Load image"))
-        LoadImg(img);
-
-    ImGui::SameLine(loadImageX + ImGui::GetItemRectSize().x + 10);
-    if (ImGui::Button("Reset image") && MsgBoxWarning("Do you really want to reset the tracking image? This change can't be undone!") == IDYES)
-        img.Reset();
-    ImGui::PopStyleColor(10);
-    ImGui::End();
-}
-
-
 int main()
 {
     const Window& window = GetWindow();
@@ -217,40 +52,29 @@ int main()
         return MsgBoxError("Failed to load monitor data");
     Image i(mInfo[0].w, mInfo[0].h);
 
-
     POINT pos{ 0,0 };
     POINT prevPos{ 1,1 };
-    bool tracking = false;
-    bool bigPixelMode = false;
-    bool sleepWhileIdle = true;
-    size_t selectedMonitor = 0;
     auto startTime = std::chrono::high_resolution_clock::now();
+    SettingsWindow sw(i);
     while (window.IsOpen())
     {
         window.StartFrame();
-        if (KeyPressed(VK_F9))
-            tracking = !tracking;
-        else if (KeyPressed(VK_F8))
-            bigPixelMode = !bigPixelMode;
-        else if (KeyPressed(VK_F7))
-            sleepWhileIdle = !sleepWhileIdle;
-
         prevPos = pos;
         if (GetCursorPos(&pos) == 0)
         {
             Err << "GetCursorPos() error: " << GetLastError() << std::endl;
         }
-        else if ((pos.x != prevPos.x || pos.y != prevPos.y) && tracking)
+        else if (sw.Tracking() && (pos.x != prevPos.x || pos.y != prevPos.y))
         {
-            i.Update(CURSOR_POS(pos.x, mInfo[selectedMonitor].x), CURSOR_POS(pos.y, mInfo[selectedMonitor].y), bigPixelMode);
+            i.Update(CURSOR_POS(pos.x, mInfo[sw.SelectedMonitor()].x), CURSOR_POS(pos.y, mInfo[sw.SelectedMonitor()].y), sw.BigPixelMode());
             startTime = std::chrono::high_resolution_clock::now();
         }
-        else if(sleepWhileIdle && std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startTime).count() > 200)
+        else if(sw.SleepWhileIdle() && std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startTime).count() > 200)
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
         ImageWindow(window.GetSize(), i);
-        SettingsWindow(window.GetSize(), pos, mInfo, tracking, bigPixelMode, sleepWhileIdle, i, selectedMonitor);
+        sw.Show(window.GetSize(), pos, mInfo);
         window.EndFrame();
     }
 }
